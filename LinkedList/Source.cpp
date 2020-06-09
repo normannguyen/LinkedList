082add0849d5ab5fd8c2ed60b1be43d971990570
Norman Nguyen <normannguyen3@gmail.com>
2020-06-09 15:20:34 -0700

    Initial

---
+++ LinkedList/Source.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+//Standard Library
+using namespace std;
+
+template <class T>
+class Node
+{
+public:
+	T data;
+	Node* next;
+	Node* prev;
+};
+
+void Display()
+{
+
+}
+void Insert()
+{
+
+}
+void Delete()
+{
+
+}
+
+//Template
+template <typename T>
+class DoubleLinkedList {
+	int main()
+	{
+		switch (choice) 
+		{
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		}
+		system("pause");
+		return 0;
+	}
+
+};
---
+++ LinkedList/doublylinkedlist.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <iostream>
+
+template <class T>
+class LinkedList
+
